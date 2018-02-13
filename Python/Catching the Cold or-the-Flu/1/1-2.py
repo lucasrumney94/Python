07476d027a20a7b8947c72b89c4b18e98c9621f5
Lucas Rumney <lucasrumney94@gmail.com>
2018-02-12 22:23:50 -0800

    add 1-2 andStringBuilder

--- Python/Catching the Cold or-the-Flu/1/1-2.py
+++ Python/Catching the Cold or-the-Flu/1/1-2.py
@@ -1,1 +1,29 @@
+def string_permutation_check(strings_to_compare):
+    character_counts = dict()
+    for my_string in strings_to_compare:
+        character_counts[my_string] = dict()
 
+        for character in my_string:
+            if character in character_counts[my_string].keys():
+                character_counts[my_string][character]+=1
+            else:
+                character_counts[my_string][character]=1
+
+    for count in character_counts:
+        if count == character_counts[strings_to_compare[0]]:
+            continue
+        else:
+            return False
+    return True
+
+
+
+
+test_strings = [
+                ["Hello","olleH"],
+                ["NoNo", "YesYes"],
+                ["ABC", "ABBC"]
+                ]
+
+for pair in test_strings:
+    print('{} and {} are Permutations: {}'.format(pair[0], pair[1], string_permutation_check(pair)))
---
+++ Python/Catching the Cold or-the-Flu/1/StringBuilder.py
@@ -0,0 +1,37 @@
+import time
+
+word_list = ["Hello ", "there! ", "How ", "are ", "you ", " doing", " today?"]
+iterations = 20000
+
+# additive string concatenation
+def join_words(words):
+    sentence = ''
+    for word in words:
+        sentence = sentence + word
+    return sentence
+
+print (join_words(word_list))
+start_time = time.time()
+for i in range(0, iterations):
+    join_words(word_list)
+end_time = time.time()
+print ('Normal Concatenation Time ' + str(end_time-start_time))
+##########################
+
+# String builder function
+def join_words_using_string_builder_idea(words):
+    sentence = []
+    for word in words:
+        sentence.append(word)
+    return ''.join(sentence)
+
+print(join_words_using_string_builder_idea(word_list))
+start_time = time.time()
+for i in range(0, iterations):
+    join_words_using_string_builder_idea(word_list)
+end_time = time.time()
+print ('String Builder Time ' + str(end_time-start_time))
+##########################
+
+
+# I think Python works differently that Java :) 
