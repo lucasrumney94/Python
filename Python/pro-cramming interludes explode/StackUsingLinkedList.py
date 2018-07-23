f0efe480e4200aab7138aff601224408c572eb4c
Lucas Rumney <lucasrumney94@gmail.com>
2018-07-22 20:18:36 -0700

    add linked list and Stack Implementation.

--- Python/pro-cramming interludes explode/StackUsingLinkedList.py
+++ Python/pro-cramming interludes explode/StackUsingLinkedList.py
@@ -5,8 +5,8 @@
 
 class Node:
     """Node for a Singly Linked List."""
-    def __init__(self, val):
-        self.val = val
+    def __init__(self, value):
+        self.value = value
         self.next = None # the pointer initially points to nothing
 
 class LinkedList:
@@ -21,41 +21,127 @@ class LinkedList:
             while current_node.next is not None:
                 string_to_print += str(current_node.value) + ' '
                 current_node = current_node.next
+            string_to_print += str(current_node.value) + ' '
             return string_to_print
         else:
-            print('empty list!')
+            return 'empty list!'
 
-    def insert(self, position, data):
-
+    def insert_after(self, index, value):
         if self.head is None:
-            self.head = Node(data)
+            self.head = Node(value)
+            return True
         else:
             current_node = self.head
-            for i in range(0, position):
+            for i in range(0, index):
                 if current_node.next is not None:
                     current_node = current_node.next
-                
+                else:
+                    raise IndexError # Should I do this?
+            new_node = Node(value)
+            new_node.next = current_node.next
+            current_node.next = new_node
+            return True
 
-    def delete(self, data):
+
+    def insert_at_head(self, value):
+        if self.head is None:
+            self.head = Node(value)
+            return True
+        else:
+            old_head = self.head
+            self.head = Node(value)
+            self.head.next = old_head
+            return True
+
+    def insert_at_tail(self, value):
         pass
 
-    def get_node(self, data):
+    def delete(self, value):
+        current_node = self.head
+        while current_node.next is not None:
+            if current_node.next.value == value:
+                break
+            else:
+                current_node = current_node.next
+        current_node.next = current_node.next.next
+
+
+    def get_node(self, value):
         pass
+
+    def delete_by_index(self, index):
+        if index == 0:
+            self.head = self.head.next
+            return
+        else:
+            current_node = self.head
+            for i in range(0, index-1):
+                if current_node.next is not None:
+                    current_node = current_node.next
+                else:
+                    raise IndexError # Should I do this??
+            current_node.next = current_node.next.next
+            return True
+
+    def get_node_index(self, value):
+        pass
+
+
 
 # A Stack needs a push and pop
 class Stack:
     """A Stack Datatype Implementation using a singly Linked List."""
     def __init__(self):
-        self.head = Node()
+        self.top = None
+        self.linked_list = LinkedList()
 
-    def push(data):
-        pass
+    def __str__(self):
+        return self.linked_list.__str__()
 
-    def pop(data):
-        pass
+    def push(self, value):
+        self.linked_list.insert_at_head(value)
+        self.top = self.linked_list.head.value
+
+    def pop(self):
+        popped_value_to_return = self.linked_list.head.value
+        self.linked_list.delete_by_index(0)
+        self.top = self.linked_list.head.value
+        return popped_value_to_return
+
+    def peek(self):
+        return self.top
+
 
 
 my_linked_list = LinkedList()
-my_linked_list.insert(0, 5)
-my_linked_list.insert(0, 7)
+
+my_linked_list.insert_at_head(4)
 print(my_linked_list)
+my_linked_list.insert_at_head(9)
+print(my_linked_list)
+my_linked_list.insert_at_head(2)
+print(my_linked_list)
+my_linked_list.insert_after(1, 5)
+print(my_linked_list)
+my_linked_list.insert_after(0, 7)
+print(my_linked_list)
+my_linked_list.insert_after(2, 9)
+print(my_linked_list)
+my_linked_list.delete(9)
+print(my_linked_list)
+my_linked_list.delete_by_index(2)
+print(my_linked_list)
+
+print('Start Stack Testing')
+
+my_stack = Stack()
+my_stack.push(5)
+print(my_stack)
+my_stack.push(6)
+print(my_stack)
+my_stack.push(8)
+print(my_stack)
+my_stack.push(9)
+print(my_stack)
+print('Popping {}'.format(my_stack.pop()))
+print(my_stack)
